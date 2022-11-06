cd1c15c060c7a644c7b7608cd24e28c4aa2a46bf
JackXuu <1018515018@qq.com>
2022-11-06 16:29:24 +0800

    jack changed

--- blog-api/src/test/java/Test1.java
+++ blog-api/src/test/java/Test1.java
@@ -13,12 +13,7 @@ public class Test1 {
 
     @Test
     public void test(){
-        TagVo tagVo1 = new TagVo("1", "11", "111");
-        TagVo tagVo2 = new TagVo("2", "11", "111");
-        List<TagVo> list = new ArrayList<>();
-        list.add(tagVo1);
-        list.add(tagVo2);
-        System.out.println(list);
+
 
     }
 }
